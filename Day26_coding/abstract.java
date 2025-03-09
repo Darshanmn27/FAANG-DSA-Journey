a57129954d28253942216fcb3f4f08d5f19b2769
darshan <153371797+Darshanmn27@users.noreply.github.com>
2025-03-10 01:33:41 +0530

    Add files via upload

---
+++ Day26_coding/abstract.java
@@ -0,0 +1,11 @@
+abstract class Animal 
+{
+    // Abstract Method (no body)
+    abstract void sound();
+
+    // Non-Abstract Method (with body)
+    void eat() 
+    {
+        System.out.println("I am eating...");
+    }
+}+
